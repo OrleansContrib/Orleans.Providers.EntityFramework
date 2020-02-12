882df24685bb14ac86cd96bce807c4a7a33cb728
Amirreza Moghassemi <amirreza.moghassemi@gmail.com>
2020-02-12 16:57:37 +0330

    Update dependencies to latest versions (#18)
    Add extensions for ISiloBuilder (for hosting orleans with .net generic host we should use this interface instead of ISiloHostBuilder)

--- src/Orleans.Providers.EntityFramework/Extensions/GrainStorageSiloHostBuilderExtensions.cs
+++ src/Orleans.Providers.EntityFramework/Extensions/GrainStorageSiloHostBuilderExtensions.cs
@@ -15,12 +15,22 @@ namespace Orleans.Providers.EntityFramework.Extensions
             string providerName)
             where TContext : DbContext
         {
-
             return builder
-                .ConfigureServices(services =>
-                {
-                    services.AddEfGrainStorage<TContext>(providerName);
-                });
+                .ConfigureServices(services => { services.AddEfGrainStorage<TContext>(providerName); });
+        }
+
+        public static ISiloBuilder AddEfGrainStorageAsDefault<TContext>(this ISiloBuilder builder)
+            where TContext : DbContext
+        {
+            return builder.AddEfGrainStorage<TContext>(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME);
+        }
+
+        public static ISiloBuilder AddEfGrainStorage<TContext>(this ISiloBuilder builder,
+            string providerName)
+            where TContext : DbContext
+        {
+            return builder
+                .ConfigureServices(services => { services.AddEfGrainStorage<TContext>(providerName); });
         }
     }
 }
